d47c06d6b2af5f228bd5cc73acecd1e41c469aeb
ueno <ueno@nayuta.co>
2019-07-25 11:44:01 +0900

    rm invoice png/txt

--- get_invoice.sh
+++ get_invoice.sh
@@ -10,10 +10,11 @@ gpio write ${LED1} ${LED_ON}
 exec 9< $0
 flock -n 9 || exit 1
 
+sudo rm -f ${PROGDIR}/invoice.png ${PROGDIR}/invoice.txt
+
 DESC=`echo $2 | sed 's/"//g'`
 ${PTARMDIR}/ptarmcli -i $1 --description="${DESC}" --no-rfield | jq -r '.result.bolt11' | tr -d '\n' > ${PROGDIR}/invoice.txt
 qrencode -s 2 -o ${PROGDIR}/invoice.png `cat ${PROGDIR}/invoice.txt`
 python3 ${EPAPERDIR}/epaper.py ${PROGDIR}/invoice.png
 
 gpio write ${LED1} ${LED_OFF}
-
