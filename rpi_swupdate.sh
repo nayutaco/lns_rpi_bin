4daf489bba2e6c2fcc5a4d8a8a28e9780239fb9f
ueno <ueno@nayuta.co>
2019-08-02 18:04:49 +0900

    move swupdate

--- rpi_swupdate.sh
+++ rpi_swupdate.sh
@@ -7,10 +7,10 @@ source /home/pi/Prog/bin/rpi_config.sh
 blink() {
 	for i in `seq 1 $1`; do
 		gpio write ${LED1} ${LED_ON}
-		gpio write ${LED2} ${LED_OFF}
+		gpio write ${LED2} ${LED_ON}
 		sleep $2
 		gpio write ${LED1} ${LED_OFF}
-		gpio write ${LED2} ${LED_ON}
+		gpio write ${LED2} ${LED_OFF}
 		sleep $2
 	done
 	gpio write ${LED1} ${LED_ON}
@@ -30,12 +30,18 @@ btn1_on=`gpio read ${BTN1}`
 btn2_on=`gpio read ${BTN2}`
 
 if [ -f ${SWUPDATE} ] && [ ${btn1_on} -eq 0 ] && [ ${btn2_on} -eq 0 ]; then
+	${EPAPERPY} "" "SW updated!" &
+	blink 20 0.1
+
+	mv ${UPDATEDIR} ${UPDATEDIR}.bak && :
 	tar jxf ${SWUPDATE} -C ${HOMEDIR}
 
 	rm ${PROGDIR}
 	ln -s ${UPDATEDIR} ${PROGDIR}
+	rm -rf ${UPDATEDIR}.bak
 	sudo rm ${SWUPDATE}
 
-	${EPAPERPY} "" "SW updated!"&
 	sudo reboot
 fi
+gpio write ${LED1} ${LED_ON}
+gpio write ${LED2} ${LED_ON}
